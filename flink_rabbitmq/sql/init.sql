a23ea567feca4984764007c0052776bbd524b93f
weiliangliang <weiliangliang@paixueche.com>
2020-01-10 13:22:15 +0800

    init.sql

---
+++ flink_rabbitmq/sql/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `message_info` (
+  `id` varchar(255) DEFAULT NULL,
+  `communication_type` varchar(255) DEFAULT NULL,
+  `communication_address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
